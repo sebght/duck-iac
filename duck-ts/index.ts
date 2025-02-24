5dd0a6a02c636883668ef9152d8cf721851f48ae
Sébastien Gahat <sebastien.gahat@octo.com>
2025-02-24 18:20:11 +0100

    usecase ecs fargate

--- duck-ts/index.ts
+++ duck-ts/index.ts
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-import {Layer} from "./tools/layer";
+import {IProgram, Layer} from "./tools/layer";
 import {Command} from "commander";
 import {SiteStatiqueWebSurS3} from "./usecases/DeployerUnSiteStatiqueSurUnBucketS3";
+import {ContainerSurEcsFargate} from "./usecases/DeployerUnContainerSurEcsFargate";
 
 const process = require('process');
 
 const command = new Command();
 
-async function initLayer(siteWeb: SiteStatiqueWebSurS3) {
-    const layer: Layer = new Layer("dev", siteWeb)
+async function initLayer(program: IProgram) {
+    const layer: Layer = new Layer("dev", program)
     await layer.init()
     await layer.installPlugins()
     await layer.setConfig([{
@@ -28,8 +29,26 @@ const deploieSiteWeb = async () => {
     console.log(`website url: ${upRes.outputs.websiteUrl.value}`);
 }
 
-const detruit = async () => {
+const deploieEcs = async () => {
+    const serviceFargate = new ContainerSurEcsFargate([{name: "aws", version: "v6.66.2"}]);
+    await serviceFargate.run()
+    const layer = await initLayer(serviceFargate);
+    await layer.refresh();
+    const upRes = await layer.up();
+    console.log(`update summary: \n${JSON.stringify(upRes.summary.resourceChanges, null, 4)}`);
+    console.log(`website url: ${upRes.outputs.url.value}`);
+}
+
+const detruitS3 = async () => {
     const siteWeb = new SiteStatiqueWebSurS3([{name: "aws", version: "v6.66.2"}]);
+    await siteWeb.run()
+    const layer = await initLayer(siteWeb);
+    await layer.refresh();
+    await layer.down();
+}
+
+const detruitEcs = async () => {
+    const siteWeb = new ContainerSurEcsFargate([{name: "aws", version: "v6.66.2"}]);
     await siteWeb.run()
     const layer = await initLayer(siteWeb);
     await layer.refresh();
@@ -57,11 +76,14 @@ command
 
 command
     .command("deploie <cible>")
-    .description("Déploie le cas d'usage ciblé (siteWeb)")
+    .description("Déploie le cas d'usage ciblé (siteS3, ecs)")
     .action((cible: string) => {
         switch (cible) {
-            case "siteWeb":
+            case "siteS3":
                 deploieSiteWeb();
+                break;
+            case "ecs":
+                deploieEcs();
                 break;
             default:
                 console.error(`Cible inconnue : ${cible}`);
@@ -71,9 +93,22 @@ command
     })
 
 command
-    .command("detruit")
-    .description("Exécute la fonction de destruction")
-    .action(detruit);
+    .command("detruit <cible>")
+    .description("Détruit l'infra ciblée (siteS3, ecs)")
+    .action((cible: string) => {
+        switch (cible) {
+            case "siteS3":
+                detruitS3();
+                break;
+            case "ecs":
+                detruitEcs();
+                break;
+            default:
+                console.error(`Cible inconnue : ${cible}`);
+                console.error("Les cibles valides sont : siteWeb");
+                process.exit(1);
+        }
+    })
 
 command
     .command("rafraichit")
---
+++ duck-ts/tools/tags.ts
@@ -0,0 +1,4 @@
+export const tags = {
+    owners: "sega/foul",
+    application: "duck-app"
+}
