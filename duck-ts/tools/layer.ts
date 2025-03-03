f0b4069c60bea465e8323cb1fbc28588dab56068
Ulysse FONTAINE (FOUL) <ulysse.fontaine@octo.com>
2025-03-03 14:03:52 +0100

    feat: add interface for layer and program

--- duck-ts/tools/layer.ts
+++ duck-ts/tools/layer.ts
@@ -1,95 +1,101 @@
-import {LocalWorkspace, PulumiFn, Stack} from "@pulumi/pulumi/automation";
+import { LocalWorkspace, PulumiFn, Stack } from "@pulumi/pulumi/automation";
+import process = require('process');
+import { IProgram } from "./program";
 
-const process = require('process');
-export interface IProgram {
-    name: () => string
-    plugins: StackDependency[]
-    run: PulumiFn
-}
 
 export type StackDependency = {
-	name: string
-	version: string
-	url?: string
+  name: string
+  version: string
+  url?: string
 }
 
 type StackConfig = {
-    stackName: string
-	projectName: string
-	plugins: StackDependency[]
+  stackName: string
+  projectName: string
+  plugins: StackDependency[]
 }
 
-type Config = {
-	key: string
-	value: string
-}
+interface ILayer<I, O> {
+  init(): void
+  installPlugins(): void
 
-interface ILayer {
-	init: () => void
-	installPlugins: () => void
-	up: () => void
-	down: () => void
-	refresh: () => void
-	preview: () => void
+  up(): void
+  down(): void
+  refresh(): void
+  preview(): void
+
+  setInputs(input: I): Promise<void>
+  getOuputs(): Promise<O>
 }
 
 function customOutput() {
-	return (output) => {
-		process.stdout.write(output)
-	};
+  return (output: any) => {
+    process.stdout.write(output)
+  };
 }
 
-export class Layer implements ILayer {
-	private stackConfig: StackConfig;
-	private program: PulumiFn;
-	private stack: Stack;
-	constructor(stackName: string, program: IProgram) {
-		this.stackConfig = {
-			stackName,
-			projectName: program.name(),
-			plugins: program.plugins
-		}
-		this.program = program.run
-	}
-    async init () {
-		this.stack= await LocalWorkspace.createOrSelectStack({
-			stackName: this.stackConfig.stackName,
-			projectName: this.stackConfig.projectName,
-			program: this.program
-		});
-	}
-    async installPlugins () {
-		console.info("installing plugins...");
-		for (const plugin of this.stackConfig.plugins) {
-			await this.stack.workspace.installPlugin(plugin.name, plugin.version);
-		}
-	}
+export class Layer<I, O> implements ILayer<I, O> {
+  private stackConfig: StackConfig;
+  private program: PulumiFn;
+  private stack: Stack;
 
-	async setConfig (configs: Config[]) {
-		console.info("setting up config");
-		for (const config of configs) {
-			await this.stack.setConfig(config.key, { value: config.value });
-		}
-	}
+  constructor(stackName: string, program: IProgram) {
+    this.stackConfig = {
+      stackName,
+      projectName: program.name(),
+      plugins: program.plugins()
+    }
+    this.program = program.run
+  }
 
-    async up () {
-		console.info("updating stack...");
-		return await this.stack.up({ onOutput: customOutput(), color: "always" });
-	}
+  async init() {
+    this.stack = await LocalWorkspace.createOrSelectStack({
+      stackName: this.stackConfig.stackName,
+      projectName: this.stackConfig.projectName,
+      program: this.program
+    });
+  }
+  async installPlugins() {
+    console.info("installing plugins...");
+    for (const plugin of this.stackConfig.plugins) {
+      await this.stack.workspace.installPluginFromServer(plugin.name, plugin.version, plugin.url);
+    }
+  }
 
-    async down () {
-		console.info("destroying stack...");
-		await this.stack.destroy({ onOutput: customOutput(), color: "always" });
-		console.info("stack destroy complete");
-	}
+  async up() {
+    console.info("updating stack...");
+    return await this.stack.up({ onOutput: customOutput(), color: "always" });
+  }
 
-    async refresh () {
-		console.info("refreshing stack...");
-		await this.stack.refresh({ onOutput: customOutput(), color: "always" });
-		console.info("refresh complete");
-	}
+  async down() {
+    console.info("destroying stack...");
+    await this.stack.destroy({ onOutput: customOutput(), color: "always" });
+    console.info("stack destroy complete");
+  }
 
-    async preview () {
-		await this.stack.preview({ onOutput: customOutput(), color: "always" })
-	}
+  async refresh() {
+    console.info("refreshing stack...");
+    await this.stack.refresh({ onOutput: customOutput(), color: "always" });
+    console.info("refresh complete");
+  }
+
+  async preview() {
+    await this.stack.preview({ onOutput: customOutput(), color: "always" })
+  }
+
+  async setInputs(inputs: I): Promise<void> {
+    const v = JSON.stringify(inputs)
+    await this.stack.setConfig("inputs", { value: v })
+  }
+
+  async getOuputs(): Promise<O> {
+    const plmOut = await this.stack.outputs()
+
+    let o: { [key: string]: any } = {}
+
+    Object.entries(plmOut).map((v) => {
+      o[v[0]] = v[1].value
+    })
+    return o as O
+  }
 }
